392c8f00bc7dbc9796591ec5beea940baab051c3
César Pérez <cesarpereziglesias@gmail.com>
2011-04-06 17:05:34 +0200

    In validator added

--- rome/validators.py
+++ rome/validators.py
@@ -6,6 +6,7 @@ class String(Validator):
     def validate(self, value):
         pass
 
+
 class Number(Validator):
 
     def validate(self, value):
@@ -19,3 +20,15 @@ class Number(Validator):
             return f_result
         except ValueError:
             raise ValidationError("This is not a number")
+
+
+class In(Validator):
+
+    def __init__(self, values, *args, **kwargs):
+        Validator.__init__(self, *args, **kwargs)
+        self._values = values
+
+    def validate(self, value):
+        if value not in self._values:
+            raise ValidationError('Value must be in list [%s]' % ', '.join(self._values))
+        return value
---
+++ tests/validators/test_in.py
@@ -0,0 +1,17 @@
+#-*- coding: utf-8 -*-
+from rome.validators import In
+
+from tests import _TestValidator
+
+class TestIn(_TestValidator):
+
+    VALUES = ['foo', 'bar']
+    VALIDATOR = In(VALUES)
+
+    def test_valid(self):
+        self.data = 'foo'
+        self.data_ok()
+
+    def test_invalid(self):
+        self.data = 'baz'
+        self.data_error('Value must be in list [%s]' % ', '.join(self.VALUES))
