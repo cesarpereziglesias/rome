9e9ca9a4628ddc2c3ed92b402e051deca622d698
Jaime Garcia <jgarcia.alfonso@gmail.com>
2011-04-07 18:15:10 +0200

    String validator completed

--- rome/validators.py
+++ rome/validators.py
@@ -3,8 +3,36 @@ from rome import Validator, ValidationError
 
 class String(Validator):
 
+    def __init__(self, *args, **kwargs):
+        Validator.__init__(self, *args, **kwargs)
+        self.max = kwargs.get('max', None)
+        self.min = kwargs.get('min', None)
+        self.empty = kwargs.get('empty', True)
+
+        if self.max is not None and self.min is not None and self.min > self.max:
+            raise ValueError("Max value must be greater than min")
+
     def validate(self, value):
-        pass
+        if not isinstance(value, basestring):
+            raise  ValidationError("This is not a String")
+
+        self.attr_validate(value)
+
+        return value
+
+    def attr_validate(self, value):
+        if not self.empty and value == '':
+            raise ValidationError('Please enter a value')
+
+        if self.max is not None:
+            if self.min == self.max and self.max != len(value):
+                raise ValidationError("Value length must be %i exactly" % self.max)
+            if len(value) > self.max:
+                raise ValidationError("Value length must be %i or less" % self.max)
+
+        if self.min is not None:
+            if len(value) < self.min:
+                raise ValidationError("Value length must be %i or more" % self.min)
 
 
 class Number(Validator):
--- tests/validators/test_string.py
+++ tests/validators/test_string.py
@@ -1,7 +1,55 @@
 # -*- coding: utf-8 -*-
+from nose.tools import assert_equals
+
 from rome.validators import String
 
 from tests import _TestValidator
 
 class TestString(_TestValidator):
-    pass
+
+    def test_valid(self):
+        self.VALIDATOR = String()
+        self.data = 'foo'
+        self.data_ok()
+
+    def test_between(self):
+        self.VALIDATOR = String(max=5, min=2, empty=False)
+        self.data = 'fooo'
+        self.data_ok()
+
+    def test_none(self):
+        self.VALIDATOR = String()
+        self.data = None
+        self.data_error('This is not a String')
+
+
+    def test_empty(self):
+        self.VALIDATOR = String(empty=False)
+        self.data = ''
+        self.data_error('Please enter a value')
+
+    def test_not_string(self):
+        self.VALIDATOR = String()
+        self.data = 9
+        self.data_error('This is not a String')
+
+    def test_wrong_interval(self):
+        try:
+            String(max=4, min=6).validate('foo')
+        except ValueError as e:
+            assert_equals('Max value must be greater than min', e.args[0])
+
+    def test_exact(self):
+        self.VALIDATOR = String(max=4, min=4)
+        self.data = 'foo'
+        self.data_error('Value length must be %(num)i exactly' % {'num': 4})
+
+    def test_wrong_max(self):
+        self.VALIDATOR = String(max=2)
+        self.data = 'foo'
+        self.data_error('Value length must be %(num)i or less' % {'num': 2})
+
+    def test_wrong_min(self):
+        self.VALIDATOR = String(min=4)
+        self.data = 'foo'
+        self.data_error('Value length must be %(num)i or more' % {'num': 4})
